fe81544e12096c5ada31a53a04f482d0c7e2ba2b
yuniquintero <yuni.quintero@gmail.com>
2019-04-20 23:28:25 -0400

    se termina problema 5

--- problema_5.py
+++ problema_5.py
@@ -41,6 +41,11 @@ def simulacion(dias):
     nuevoTanque = generarTipoDeTanque()
     colaTanques.append(nuevoTanque)
 
+    arr_tanques_en_puerto = []
+    arr_tanques_llegada = []
+    arr_terminal_A = []
+    arr_terminal_B = []
+
     while n < dias:
         tiempoRestanteA -= 1
         tiempoRestanteB -= 1
@@ -71,29 +76,74 @@ def simulacion(dias):
             tanquesEnPuerto += 1
 
         n += 1
-    return cantidadTanques, tanquesEnPuerto, tiempoDesocupadoA, tiempoDesocupadoB
+        arr_tanques_en_puerto.append(tanquesEnPuerto)
+        arr_tanques_llegada.append(cantidadTanques)
+        arr_terminal_A.append(tiempoDesocupadoA)
+        arr_terminal_B.append(tiempoDesocupadoB)
+    return cantidadTanques, tanquesEnPuerto, tiempoDesocupadoA, tiempoDesocupadoB, arr_tanques_en_puerto, arr_tanques_llegada, arr_terminal_A, arr_terminal_B
 
 dias = 1000
-cantidadTanques, tanquesEnPuerto, tiempoDesocupadoA, tiempoDesocupadoB = simulacion(dias)
+cantidadTanques, tanquesEnPuerto, tiempoDesocupadoA, tiempoDesocupadoB, arr_tanques_en_puerto, arr_tanques_llegada, arr_terminal_A, arr_terminal_B = simulacion(dias)
 
 def get_intervalo(promedio, delta):
     return "[" + str(promedio - delta) + ", " + str(promedio + delta) + "]"
 
 def print_resultado(promedio, intervalo, msg):
     print("Promedio de " + msg + ": " + str(promedio))
-    print("Intervalo de confianza: " + intervalo)
+    print("Intervalo de confianza 95%: " + intervalo)
     print("----------------------------------------------------------------")
 
-promedio_tanques = cantidadTanques / dias
-print("Promedio de tanques: " + str(promedio_tanques))
-
 promedio_libre_a = tiempoDesocupadoA / dias
-print("Promedio de desocupado A: " + str(promedio_libre_a))
 
 promedio_libre_b = tiempoDesocupadoB / dias
 print("Promedio de desocupado B: " + str(promedio_libre_b))
 
 
-
+# promedio tanques por dia en puerto
 promedio_tanques_puerto = tanquesEnPuerto / dias
+cuadrados = []
+for tanque in arr_tanques_en_puerto:
+    r = (tanque - promedio_tanques_puerto)**2
+    cuadrados.append(r)
+
+sd = (sum(cuadrados)/(dias-1))**0.5
+delta = 1.96 * (sd/(dias**0.5))
+int_conf = get_intervalo(promedio_tanques_puerto,delta)
+print_resultado(promedio_tanques_puerto, int_conf, "tanques en el puerto")
+
+# promedio de dias que pasa un tanque en el puerto
+promedio_tanques = cantidadTanques / dias
+cuadrados = []
+for tanque in arr_tanques_llegada:
+    r = (tanque - promedio_tanques)**2
+    cuadrados.append(r)
+
+sd = (sum(cuadrados)/(dias-1))**0.5
+delta = 1.96 * (sd/(dias**0.5))
+int_conf = get_intervalo(promedio_tanques,delta)
+print_resultado(promedio_tanques, int_conf, "tanques que llegan")
+
+# promedio de tiempo desocupado terminal A
+promedio_libre_a = tiempoDesocupadoA / dias
+cuadrados = []
+for tanque in arr_terminal_A:
+    r = (tanque - promedio_libre_a)**2
+    cuadrados.append(r)
+
+sd = (sum(cuadrados)/(dias-1))**0.5
+delta = 1.96 * (sd/(dias**0.5))
+int_conf = get_intervalo(promedio_libre_a,delta)
+print_resultado(promedio_libre_a, int_conf, "de tiempo desocupado terminal A")
+
+# promedio de tiempo desocupado terminal B
+promedio_libre_b = tiempoDesocupadoB / dias
+cuadrados = []
+for tanque in arr_terminal_B:
+    r = (tanque - promedio_libre_b)**2
+    cuadrados.append(r)
+
+sd = (sum(cuadrados)/(dias-1))**0.5
+delta = 1.96 * (sd/(dias**0.5))
+int_conf = get_intervalo(promedio_libre_b,delta)
+print_resultado(promedio_libre_b, int_conf, "de tiempo desocupado terminal B")
 
